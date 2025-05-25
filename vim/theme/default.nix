a61778983dbf5b07cec4b52a46d6bdb80cbac04b
Permafrozen <m8tt8o@gmail.com>
2025-05-25 11:22:14 +0200

    added transparency for the tabline

--- vim/theme/default.nix
+++ vim/theme/default.nix
@@ -22,5 +22,8 @@
     vim.api.nvim_set_hl(0, 'Pmenu',       { bg = 'none' })
     vim.api.nvim_set_hl(0, "StatusLine",  { bg = "none" })
     vim.api.nvim_set_hl(0, "StatusLineNC",{ bg = "none" })
+    vim.api.nvim_set_hl(0, "TabLine",     { bg = "none" })
+    vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
+    vim.api.nvim_set_hl(0, "TabLineSel",  { bg = "none" })
   '';
 }
